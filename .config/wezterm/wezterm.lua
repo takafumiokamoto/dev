1c69c9fecfab532f21a5e8e69249fe752dc8a45f
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2025-06-28 17:02:53 +0900

    20250628:17:02:53

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,25 +1,24 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.font =
-  wezterm.font('CaskaydiaMono NFM', { weight = 'Bold', italic = true })
--- config.default_domain = 'WSL:Ubuntu'
-config.default_prog = { 'pwsh.exe' }
+config.font = wezterm.font("CaskaydiaMono NFM", { weight = "Bold", italic = true })
+config.default_domain = "WSL:Ubuntu"
+-- config.default_prog = { "pwsh.exe" }
 config.hide_tab_bar_if_only_one_tab = true
 config.use_ime = true
 -- config.color_scheme = 'Catppuccin Frappe'
-config.color_scheme = 'Palenight (Gogh)'
+config.color_scheme = "Palenight (Gogh)"
 config.font_size = 12
-config.window_close_confirmation = 'AlwaysPrompt'
+config.window_close_confirmation = "AlwaysPrompt"
 config.enable_scroll_bar = true
 config.initial_rows = 36
 config.initial_cols = 120
-config.default_cursor_style = 'BlinkingUnderline'
-config.audible_bell = 'Disabled' 
+config.default_cursor_style = "BlinkingUnderline"
+config.audible_bell = "Disabled"
 
 return config
--- makefile
+++ makefile
@@ -1,13 +1,40 @@
+.DEFAULT_GOAL := all
+SHELL := /bin/bash
+NUM_THREADS?=1
+
 ifeq ($(OS),Windows_NT)
 	CURRENT_DATETIME := $(shell powershell -Command "Get-Date -Format 'yyyyMMdd:HH:mm:ss'")
 else
 	CURRENT_DATETIME := $(shell date +%Y%m%d:%H:%M:%S)
 endif
+
 .PHONY: c ## Create a new commit with the current date and time
 c:
 	git add .
 	git commit -m "$(CURRENT_DATETIME)"
 	git push
+
+.PHONY: .uv  ## Check dependency
+.uv:
+	@uv -V || echo 'Please install uv: https://docs.astral.sh/uv/getting-started/installation/'
+
+.PHONY: .aqua
+	@aqua version || echo 'Please install aqua: https://aquaproj.github.io/docs/install'
+
+.PHONY: .d ## Check dependency
+dependency: .uv .aqua
+		@echo 'ok'
+
+.PHONY: cli ## Install CLI tools
+cli: .d
+	aqua i
+
+.PHONY: config ## Install Configuration
+cli: .d
+	uv run --script scripts/install.py
+
+.PHONY: all ## Install All
+all: .d cli config
 
 .PHONY: help  ## Display this message
 help:
