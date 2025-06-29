f395b7b141f56173ffd8ba67a14385717edce2dc
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2025-06-30 01:53:06 +0900

    20250630:01:53:06

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,14 +1,8 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
-local keymap = vim.keymap
-
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+vim.keymap.set("v", "<C-c>", [["+y]])
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 -- keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,12 +1,12 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+--vim.opt.clipboard = ""
 vim.o.termguicolors = true
 vim.g.have_nerd_font = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.mouse = "a"
 vim.opt.showmode = false
-vim.opt.clipboard = ""
 vim.opt.breakindent = true
 vim.opt.undofile = true
 vim.opt.ignorecase = true
