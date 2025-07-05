d704ddd6bc143c89c2d862285a21933da890d757
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2025-07-06 04:17:13 +0900

    20250706:04:17:13

--- .config/nvim/lua/core/neovide.lua
+++ .config/nvim/lua/core/neovide.lua
@@ -1,3 +1,6 @@
 vim.o.guifont = "CaskaydiaMono NFM:i:h11"
 vim.keymap.set({ "n", "v" }, "<C-v>", [["+p]])
 vim.keymap.set("i", "<C-v>", "<C-r>+")
+vim.g.neovide_title_background_color =
+	string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
+vim.g.neovide_title_text_color = "#C792EA"
