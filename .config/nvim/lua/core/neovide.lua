a15bd7cbe5752abc9459d68442f8cdbca9aabd66
Takafumi Okamoto <takafumiokamoto.dev@gmail.com>
2025-07-07 01:37:30 +0900

    20250707:01:37:30

--- .config/nvim/lua/core/neovide.lua
+++ .config/nvim/lua/core/neovide.lua
@@ -4,3 +4,11 @@ vim.keymap.set("i", "<C-v>", "<C-r>+")
 vim.g.neovide_title_background_color =
 	string.format("%x", vim.api.nvim_get_hl(0, { id = vim.api.nvim_get_hl_id_by_name("Normal") }).bg)
 vim.g.neovide_title_text_color = "#C792EA"
+-- vim.g.neovide_fullscreen = true
+vim.g.neovide_input_ime = true
+vim.g.neovide_cursor_animate_in_insert_mode = true
+vim.g.neovide_cursor_animate_command_line = true
+vim.g.neovide_cursor_smooth_blink = true
+vim.g.neovide_cursor_vfx_mode = "railgun"
+-- vim.g.neovide_cursor_vfx_mode = "torpedo"
+-- vim.g.neovide_cursor_vfx_mode = "pixiedust"
